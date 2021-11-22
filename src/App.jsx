1a5003832ee4397eec8ebc1e0b812ddea632a2fa
pedrodruviaro <pedrodruviaro@gmail.com>
2021-11-22 09:45:10 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export const App = () => {
+    return (
+        <>
+            <h1>React App</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+                nemo!
+            </p>
+            <button>Primary</button>
+            <button>Secondary</button>
+        </>
+    );
+};
